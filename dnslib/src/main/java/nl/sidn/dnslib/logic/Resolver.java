f4389a999032df997ef4409881b86f6e16474214
Maarten Wullink <maarten.wullink@sidn.nl>
2013-04-11 11:20:59 +0200

    improved performance

--- dnslib/src/main/java/nl/sidn/dnslib/logic/Resolver.java
+++ dnslib/src/main/java/nl/sidn/dnslib/logic/Resolver.java
@@ -4,8 +4,6 @@ import nl.sidn.dnslib.logic.unbound.UnboundLibrary;
 import nl.sidn.dnslib.logic.unbound.ub_result;
 import nl.sidn.dnslib.message.Message;
 import nl.sidn.dnslib.message.util.NetworkData;
-import nl.sidn.dnslib.trustanchor.TrustAnchor;
-import nl.sidn.dnslib.trustanchor.TrustAnchorService;
 import nl.sidn.dnslib.types.RcodeType;
 import nl.sidn.dnslib.types.ResourceRecordClass;
 import nl.sidn.dnslib.types.ResourceRecordType;
@@ -16,79 +14,19 @@ public class Resolver {
 	
 	private Pointer<UnboundLibrary.ub_ctx> ctx;
 		
-	private static TrustAnchor ta;
-	
-	private static String ds;
-	
-	static{
-		init();
-	}
-	
-	private static synchronized void init() {
-		if(ta == null){
-			/* load root trust anchor */
-			TrustAnchorService tas = new TrustAnchorService();
-			ta = tas.loadTrustAnchor();
-			//create ds record which is used by libunbound as ta for the root.
-			ds = ". IN DS " + ta.getKeyDigest().getKeyTag() + " " + ta.getKeyDigest().getAlgorith() + " " + ta.getKeyDigest().getDigestType() + 
-					" " + ta.getKeyDigest().getDigest();
-		}
-	}
-	
 	public Resolver(){
 		ctx = UnboundLibrary.ub_ctx_create();
 		//start with iterator only
-		addOption("module-config:", "iterator");
-	}
-	
-	public enum DEBUGMODE {OFF, MINIMAL, VERBOSE, EXTREME};
-	
-	public void setDebug(DEBUGMODE mode) {
-		if(mode == DEBUGMODE.OFF){
-			UnboundLibrary.ub_ctx_debuglevel(ctx, 0);
-		}else if(mode == DEBUGMODE.MINIMAL){
-			UnboundLibrary.ub_ctx_debuglevel(ctx, 1);
-		}else if(mode == DEBUGMODE.VERBOSE){
-			UnboundLibrary.ub_ctx_debuglevel(ctx, 2);
-		}else if(mode == DEBUGMODE.EXTREME){
-			UnboundLibrary.ub_ctx_debuglevel(ctx, 3);
-		}
+		ResolverContextBuilder builder =  new ResolverContextBuilder();
+		ctx = builder.withIterator().build().getCtx();
 		
-		addOption("logfile:", "/var/log/libunbound.log");
 	}
 	
-	private void addOption(String option, String value){
-		int status = UnboundLibrary.ub_ctx_set_option(ctx, Pointer.pointerToCString(option), Pointer.pointerToCString(value));
-		if (status != 0) {
-			throw new RuntimeException("Unable to add option to libunbound");
-		}
+	public Resolver(Context ctx){
+		this.ctx = ctx.getCtx();
 	}
 	
-	public void setDnsSecEnabled(){
-		addOption("module-config:", "validator iterator");
-		addOption("edns-buffer-size:", "4096");
-		
-		int status = UnboundLibrary.ub_ctx_add_ta(ctx, Pointer.pointerToCString(ds));
-		if (status != 0) {
-			throw new RuntimeException("Unable to add trust anchor to libunbound");
-		}
-	}
-	
-	public void setTCPEnabled(){
-		addOption("tcp-upstream:", "yes");
-	}
-	
-	public void setEdnsBufferSize(String size){
-		addOption("edns-buffer-size:", size);
-	}
 
-	public void setForwardingServer(String fwd){
-		int status = UnboundLibrary.ub_ctx_set_fwd(ctx, Pointer.pointerToCString(fwd));
-		if (status != 0) {
-			throw new RuntimeException("Unable to set forwarding server for libunbound");
-		}
-	}
-	
 	public LookupResult lookup(String qName, ResourceRecordType qType, ResourceRecordClass qClazz, boolean decode ){
 
 		Pointer<Byte> name = Pointer.pointerToCString(qName);
@@ -96,7 +34,7 @@ public class Resolver {
 		Pointer<Pointer<ub_result>> result = Pointer.allocatePointer(ub_result.class);
 		int status = UnboundLibrary.ub_resolve(ctx, name, qType.getValue(), qClazz.getValue(), result);
 				
-		UnboundLibrary.ub_ctx_delete(ctx);
+		//UnboundLibrary.ub_ctx_delete(ctx);
 		 
 		LookupResult lr = new LookupResult();
 		if(status == 0){
