8db19ee7f98cd03df550905cf725838c9c7f0ff6
Maarten Wullink <maarten.wullink@sidn.nl>
2013-07-22 16:14:15 +0200

    openssl lock

--- dnslib/src/main/java/nl/sidn/dnslib/logic/openssl/impl/LockingCallback.java
+++ dnslib/src/main/java/nl/sidn/dnslib/logic/openssl/impl/LockingCallback.java
@@ -7,9 +7,17 @@ import java.util.concurrent.Semaphore;
 import nl.sidn.dnslib.logic.openssl.wrapper.LibCryptoLibrary;
 import nl.sidn.dnslib.logic.openssl.wrapper.LibCryptoLibrary.CRYPTO_set_locking_callback_func_callback;
 
+import org.apache.log4j.Logger;
 import org.bridj.Pointer;
 
+/**
+ * Callback for the openssl library. openssl will call the callback whenever
+ * it needs to lock a threads for some time.
+ *
+ */
 public class LockingCallback extends CRYPTO_set_locking_callback_func_callback {
+	
+	private static final Logger LOGGER = Logger.getLogger(LockingCallback.class);
 
 	private List<Semaphore> locks = new ArrayList<>();
 	
@@ -18,18 +26,19 @@ public class LockingCallback extends CRYPTO_set_locking_callback_func_callback {
 		for (int i = 0; i < lockCount; i++) {
 			locks.add(new Semaphore(1));
 		}
+		LOGGER.info("Initialized OpenSSL callback with " + lockCount + " locks");
 	}
 	
 	@Override
-	public void apply(int mode, int type, Pointer<Byte> file, int line) {
+	public void apply(int mode, int locknumber, Pointer<Byte> file, int line) {
 		if (mode == LibCryptoLibrary.CRYPTO_LOCK){
 			try {
-				locks.get(type).acquire();
+				locks.get(locknumber).acquire();
 			} catch (InterruptedException e) {
 				throw new RuntimeException("Interupted while acquiring lock", e);
 			}
 		} else{
-			locks.get(type).release();
+			locks.get(locknumber).release();
 		}
 	}
 
--- dnslib/src/main/java/nl/sidn/dnslib/logic/openssl/impl/OpenSSL.java
+++ dnslib/src/main/java/nl/sidn/dnslib/logic/openssl/impl/OpenSSL.java
@@ -3,6 +3,7 @@ package nl.sidn.dnslib.logic.openssl.impl;
 import nl.sidn.dnslib.logic.openssl.wrapper.LibCryptoLibrary;
 import nl.sidn.dnslib.logic.openssl.wrapper.LibCryptoLibrary.CRYPTO_set_locking_callback_func_callback;
 
+import org.apache.log4j.Logger;
 import org.bridj.BridJ;
 import org.bridj.Pointer;
 
@@ -13,10 +14,13 @@ import org.bridj.Pointer;
  */
 public class OpenSSL {
 	
+	private static final Logger LOGGER = Logger.getLogger(OpenSSL.class);
+	
 	private static LibCryptoLibrary.CRYPTO_set_id_callback_func_callback idcb;
 	private static CRYPTO_set_locking_callback_func_callback LockingCallback;
 	
 	public static void init(){
+		LOGGER.info("Initialize OpenSSL"); 
 		idcb = new IdCallback();
 		LibCryptoLibrary.CRYPTO_set_id_callback(Pointer.pointerTo(idcb));
 		/* make sure the callbacks do not get garbage collected */
@@ -29,6 +33,7 @@ public class OpenSSL {
 	}
 	
 	public static void cleanup(){
+		LOGGER.info("Cleanup OpenSSL");
 		BridJ.unprotectFromGC(idcb);
 		BridJ.unprotectFromGC(LockingCallback);
 	}
